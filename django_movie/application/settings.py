4aeaebeebeeee1be1bf7fe3dbfc8cba811ecb499
vseop <ivpython@mail.ru>
2022-06-28 17:18:23 +0300

    update settings, README

--- django_movie/application/settings.py
+++ django_movie/application/settings.py
@@ -76,16 +76,7 @@ WSGI_APPLICATION = 'application.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'movie',
-#         'USER': 'postgres',
-#         'PASSWORD': '******',
-#         'HOST': 'localhost',
-#         'PORT': '5433',
-#     }
-# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
